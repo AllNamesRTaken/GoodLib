9937eab10da362114e67fae0715e156e81c256cc
joel.brage <joel.brage@unit4.com>
2018-03-21 14:00:01 +0100

    added webpackExternals.js and moved dts files into dts folder

---
+++ src/lib/webpackExternals.js
@@ -0,0 +1,38 @@
+const rootPatterns = [{
+    regex: /^YourLibraryName\/([A-Z].*)/,
+    root: ['YourLibraryName', '']
+  }];
+  
+  function rootForRequest(path) {
+    const match = rootPatterns.find((pattern, index, obj) => !!path.match(pattern.regex));
+  
+    if (match) {
+      let m = path.match(match.regex);
+      if (m.length > 1) {
+        match.root[match.root.length - 1] = m[m.length - 1];
+      }
+      return [...match.root];
+    }
+  
+    return 'YourLibraryName';
+  }
+  
+  function libraryExternalsFactory() {
+  
+    return function libraryExternals(context, request, callback) {
+  
+      if (request.startsWith('YourLibraryName/')) {
+        return callback(null, {
+          root: rootForRequest(request),
+          commonjs: request,
+          commonjs2: request,
+          amd: request
+        });
+      }
+  
+      callback();
+  
+    };
+  
+  }
+  module.exports = libraryExternalsFactory;
